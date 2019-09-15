f8c43e52292187d70bfe51a7eb713d5b8e05d06e
Klaudia <klaudia.goska06@gmail.com>
2019-09-15 11:03:33 +0200

    possibility to sign in

---
+++ app/src/main/java/com/example/mat_k/hack2k19/SignInLayout.java
@@ -0,0 +1,19 @@
+package com.example.mat_k.hack2k19;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SignInLayout extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin_layout);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+}
